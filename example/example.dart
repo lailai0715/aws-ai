020f9e764768caf8bcdc383e079737ab76cb1f42
Lai Zong Hao <lailai1314520@gmail.com>
2022-09-19 16:28:42 +0800

    Removed: http package

--- example/example.dart
+++ example/example.dart
@@ -1,22 +1,17 @@
 import 'package:aws_ai/src/RekognitionHandler.dart';
 import 'package:aws_ai/src/TranslateHandler.dart';
-import 'dart:async';
 import 'dart:io';
 
 main() async {
-
   File sourceImagefile; //load source image in this File object
-  String  accessKey = "",
-          secretKey = "",
-          region    = "" ;
+  String accessKey = "", secretKey = "", region = "";
 
   RekognitionHandler rekognition = new RekognitionHandler(accessKey, secretKey, region);
   String labelsArray = await rekognition.detectLabels(sourceImagefile);
   print(labelsArray);
 
-
-
   TranslateHandler translate = new TranslateHandler(accessKey, secretKey, region);
-  String output = await translate.translate(TranslateLanguages.ar, TranslateLanguages.en, "اسمي محمد");
+  String output =
+      await translate.translate(TranslateLanguages.ar, TranslateLanguages.en, "اسمي محمد");
   print(output);
 }
